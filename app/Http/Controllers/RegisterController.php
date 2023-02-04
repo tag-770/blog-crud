dd42d5d67687760686466daef22d094b29a635ac
tag-770 <eeee.chanarivi@icloud.com>
2023-02-04 09:51:32 +0900

    PHPDocsを追加した

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
+    /**
+     * 会員登録ページを表示する
+     */
     public function index()
     {
         return view("register");
