e99764ed56e3f5dea56af02e09eb6a274fdcc5b7
tag-770 <eeee.chanarivi@icloud.com>
2023-03-04 16:04:27 +0900

    ブログの編集画面を表示する際のユーザー判定機能を追加した

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -74,6 +74,11 @@ class BlogController extends Controller
      */
     public function edit($id)
     {
+        $login_user_id = Auth::id();
+        $blog_user_id = Blog::find($id)->user_id;
+        if ($login_user_id !== $blog_user_id) {
+            abort(403);
+        }
         $blog = Blog::find($id);
         return view("edit")->with('blog', $blog);
     }
