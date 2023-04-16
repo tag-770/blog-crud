364110ff093018d3b6a7cb28bf667974215b124b
tag-770 <eeee.chanarivi@icloud.com>
2023-04-16 21:30:24 +0900

    カテゴリーを作成した

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * カテゴリー名の作成画面を表示する
+     */
+    public function createCategory()
+    {
+        return view("category");
+    }
+
+    /**
+     * カテゴリーを作成する
+     */
+    public function storeCategory(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'slug' => ['required', 'unique:categories'],
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+            'slug' => $request->slug,
+        ]);
+
+        return redirect()->route('home');
+    }
+}
