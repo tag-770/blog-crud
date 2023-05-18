d11ab48ffb113f3e34a72e00d69ec110c89273b2
tag-770 <eeee.chanarivi@icloud.com>
2023-05-19 00:12:05 +0900

    リレーション経由で自身のブログを取得した

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     *自分のブログを取得
+     */
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
