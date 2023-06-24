77abbb6ef3472d44116a97e00f377536d6d1e496
tag-770 <eeee.chanarivi@icloud.com>
2023-06-24 22:32:25 +0900

    /homeに全投稿一覧を表示した

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,5 +10,11 @@
     <h1>Blog CRUD</h1>
     <a href="{{ route('blog.create') }}" class="btn"> ブログを登録する </a>
     <a href="{{ route('logout') }}" class="btn"> ログアウトする </a>
+    <h1>全投稿一覧</h1>
+        @foreach($blogs as $blog)
+            <div>
+                <p>{{$blog->title}} - {{$blog->body}}</p>
+            </div>
+        @endforeach
 </body>
 </html>
