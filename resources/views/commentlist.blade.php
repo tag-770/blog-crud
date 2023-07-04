4043a210a2f3fb72897352d8067149c4927ed384
tag-770 <eeee.chanarivi@icloud.com>
2023-07-04 21:06:23 +0900

    /mypage/commentsでコメントがないときのメッセージを表示

--- resources/views/commentlist.blade.php
+++ resources/views/commentlist.blade.php
@@ -7,11 +7,16 @@
     </head>
     <body>
         <h1>コメント一覧</h1>
+        @if (!$comments->isEmpty())
         @foreach($comments as $comment)
             <div>
                 <p>{{$comment->body}} - {{$comment->created_at}}</p>
                 <a href="{{ route('blog.show', ['id'=>$comment->blog->id]) }}">{{$comment->blog->title}}</a>
             </div>
         @endforeach
+        @else
+             コメントはありません
+        @endif
+        
     </body>
 </html>        
